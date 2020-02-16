631e141dadd8b0a46094e73073af92b3a28f6b19
AhmedEhsouna <ahmedehsouna@yahoo.com>
2020-02-16 20:13:02 +0100

    commited

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ module.exports = {
     Dislike: require('./dislike.js'),
     Like: require('./like.js'),
     Post: require('./post.js'),
-    Event: require(path.resolve(__dirname, 'event.js')),
+    Event: require('./event.js'),
     Following: require('./following.js'),
     Participation : require('./participation.js'),
     Community: require('./community.js'),
